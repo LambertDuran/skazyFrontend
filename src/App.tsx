c7f8952a7a76ebfc0defb65a97073e5e5ef05e16
lambert duran <lambert.duran@agilesoft.nc>
2024-06-09 12:35:47 +1100

    Ajout du filtre

--- src/App.tsx
+++ src/App.tsx
@@ -13,6 +13,7 @@ import ListItemText from '@mui/material/ListItemText';
 import {generateSolutions, deleteSolution, deleteSolutions} from "./services/solutionServices";
 import Serpentin from "./components/Serpentin";
 import Pagination from '@mui/material/Pagination';
+import {TextField} from '@mui/material';
 
 function App() {
 
@@ -24,6 +25,7 @@ function App() {
     const [solutions, setSolutions] = useState<Solution[] | null>(null);
     const [solution, setSolution] = useState<Solution | null>(null);
     const [page, setPage] = useState(1);
+    const [filteredSolutions, setFilteredSolutions] = useState<Solution[] | null>(null);
 
     async function HandleClickGenerate() {
         setIsLoading(true);
@@ -35,6 +37,7 @@ function App() {
                 setTime(data.time);
                 const fetchedSolutions = data.solutions;
                 setSolutions(fetchedSolutions);
+                setFilteredSolutions(fetchedSolutions);
                 if (fetchedSolutions && fetchedSolutions.length > 0) {
                     setSolution(fetchedSolutions![0])
                 }
@@ -48,15 +51,32 @@ function App() {
                 setTime(null);
                 setSolution(null);
                 setSolutions(null);
+                setFilteredSolutions(null);
             });
     }
 
-    async function HandleClickDelete(id: number, solutions: Solution[]) {
+    async function HandleClickDelete(id: number) {
+        if(!solutions) return;
         const response = await deleteSolution(id)
             .then(s => {
                 deleteSolution(id)
                 setSolutions(solutions.filter(s => s.id !== id));
+                if(filteredSolutions) setFilteredSolutions(filteredSolutions.filter(s => s.id !== id));
             })
+    }
+
+    function handleChange (event: any) {
+        const search = event.target.value;
+        console.log('search', search);
+        if(!search) {
+            setFilteredSolutions(solutions);
+            return;
+        }
+        const searchStr = search.split('').join(',');
+        const filtered = solutions?.filter(s => {
+            return s.unknowns.join(",").includes(searchStr);
+        })
+        setFilteredSolutions(filtered ?? null);
     }
 
     // @ts-ignore
@@ -67,6 +87,31 @@ function App() {
             </div>
             <div className='solutions'>
                 <div>
+                    <TextField
+                        label="Recherche"
+                        variant="standard"
+                        className="recherche"
+                        onChange={handleChange}
+                        sx={{
+                            '& .MuiInput-underline:before': {
+                                borderBottomColor: '#1976d2', // Couleur des contours avant focus
+                            },
+                            '& .MuiInput-underline:hover:before': {
+                                borderBottomColor: '#1976d2', // Couleur des contours au survol
+                            },
+                            '& .MuiInput-underline:after': {
+                                borderBottomColor: '#1976d2', // Couleur des contours après focus
+                            },
+                            '& .MuiInputLabel-root': {
+                                color: '#1976d2', // Couleur du label
+                            },
+                            '& .MuiInputLabel-root.Mui-focused': {
+                                color: '#1976d2', // Couleur du label au focus
+                            },
+                            '& .MuiInputBase-input': {
+                                color: 'white', // Couleur du texte
+                            },
+                        }}/>
                     <Button
                         variant="outlined"
                         onClick={HandleClickDeleteAll}
@@ -80,7 +125,7 @@ function App() {
                 </div>
                 {time && <p>Temps de calcul : {time}s </p>}
                 <List>
-                    {solutions && solutions
+                    {filteredSolutions && filteredSolutions
                         .slice((page - 1) * nbElementsByPage, page * nbElementsByPage)
                         .map((sol: Solution) =>
                             <ListItem disablePadding key={sol.id}>
@@ -97,14 +142,14 @@ function App() {
                                 </ListItemButton>
                                 <Button variant="contained"
                                         onClick={() => {
-                                            HandleClickDelete(sol.id, solutions)
+                                            HandleClickDelete(sol.id)
                                         }}>
                                     x
                                 </Button>
                             </ListItem>)}
                 </List>
-                {solutions?.length &&
-                    <Pagination count={Math.floor(solutions!.length / nbElementsByPage + 1)}
+                {filteredSolutions?.length &&
+                    <Pagination count={Math.floor(filteredSolutions!.length / nbElementsByPage + 1)}
                                 color="primary"
                                 onChange={(event: React.ChangeEvent<unknown>,
                                            page: number) => {
